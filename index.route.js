0906da138b4077f2f488e48c5e59667fb29d9a9e
TarunKaradiya <karadiyatarun0@gmail.com>
2023-03-29 17:35:06 +0530

    saddsads

---
+++ index.route.js
@@ -0,0 +1,14 @@
+//import express from "express";
+import { indexPage, signinPage, signupPage, signup, signin, signout } from "../controller/index.controller.js";
+import { verify } from "../middleware/authenticate.js";
+const   router = express.Router();
+
+// htto://localhost:3001/
+router.get("/",indexPage);
+router.get("/signin",signinPage);
+router.get("/signup",signupPage);
+router.post("/signup",signup);
+router.post("/signin",signin);
+router.get("/signout",verify,signout);
+
+export default router;
